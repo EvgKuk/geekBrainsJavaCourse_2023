1b5bbae9d2a661715ea1e8eabd5e8ab17637e2bf
Evgeny Kukushkin <evg.kuk.prm@gmail.com>
2023-05-13 11:11:59 +0500

    Добавлены домашние задания к Уроку 6

---
+++ src/homeworks/seminar6/BaseOfNotebook.java
@@ -0,0 +1,12 @@
+package homeworks.seminar6;
+
+/**
+ * Каталог ноутбуков
+ */
+public class BaseOfNotebook extends Notebook {
+
+    public BaseOfNotebook(int ram, int ssd, String os, String color){
+        super(ram,ssd,os,color);
+    }
+
+}
